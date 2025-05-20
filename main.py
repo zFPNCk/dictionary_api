a7508fa61dfb5a010aac2aaa6ce2abded1ff3f44
Nicolas <nicolasguimafpnc@gmail.com>
2025-05-20 19:39:51 -0300

    First commit

---
+++ main.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/api/v1/<word>')
+def word(definition, word):
+    definition = word.upper
+    return {"definition": definition,
+            "word": word}
+
+if __name__ == '__main__':
+    app.run(debug=True)
